452c6dd76b3acb65342ffa5aa511ed40b052d020
IamAmeySalvi109 <slavyamey@gmail.com>
2019-08-20 00:03:56 -0400

    Add files via upload

---
+++ Assignment_2/Assignment_2_Part_1.py
@@ -0,0 +1,18 @@
+x = [1,2,3,4,[10,20,30,40,[100,200,300,400],"rishabh_",5+5j],4000]
+print("Given List: ",x)
+
+print()
+
+print (x[4][:2])
+
+print()
+
+print (x[4][6])
+
+print()
+
+print (x[4][4][2:4])
+
+print()
+
+print (x[4][3:6])
--- Assignment_2/Assignment_2_Part_3.py
+++ Assignment_2/Assignment_2_Part_3.py
@@ -1,18 +1,24 @@
-x = "Hello&$$world"
+import re
+from collections import Counter
+x = "Hello&*$$world"
 
 print("My string: ",x)  
 
-y = {} 
+#y = {} 
 
-for i in x: 
-	if i in y: 
-		y[i] += 1
-	else: 
-		y[i] = 1
+#for i in x: 
+#	if i in y: 
+#		y[i] += 1
+#	else: 
+#		y[i] = 1
 
-print (y)
+#print (y)
 
-for key,value in y.items():
-	if ((key>='a' and key<='z')or(key>='A'and key<='Z')or(key>='0'and key<='9')):
-		del y[key,value]
+#for key,value in y.items():
+#	if ((key>='a' and key<='z')or(key>='A'and key<='Z')or(key>='0'and #key<='9')):
+#		del y[key,value]
+#print(y)
+
+z = re.findall('[^A-Za-z0-9]',x)
+y = Counter(z)
 print(y)
