313fcc1445729c600f6085369b695ba246cf3b9c
IamAmeySalvi109 <slavyamey@gmail.com>
2019-08-20 00:07:19 -0400

    Add files via upload

---
+++ Task_2_Aug13.py
@@ -0,0 +1,14 @@
+x=18
+y=20
+z=14
+
+print ("X:", x)
+print ("Y:", y)
+print ("Z:", z)
+
+if (x>y) and (x>z):
+	print ("X is the Greatest Value")
+elif (y>x) and (y>z):
+	print ("Y is the Greatest Value")
+else:
+	print ("Z is Greatest Value")
