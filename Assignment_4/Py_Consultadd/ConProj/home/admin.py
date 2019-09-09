ffcdddeba9c63a27dc9533546ac9d2725362a6f3
IamAmeySalvi109 <slavyamey@gmail.com>
2019-09-08 20:44:17 -0400

    Added more models

--- Assignment_4/Py_Consultadd/ConProj/home/admin.py
+++ Assignment_4/Py_Consultadd/ConProj/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Example, Club, Movie, Game
 
 # Register your models here.
+admin.site.register(Example)
+
+admin.site.register(Club)
+
+admin.site.register(Movie)
+
+admin.site.register(Game)
--- Assignment_4/Py_Consultadd/ConProj/home/models.py
+++ Assignment_4/Py_Consultadd/ConProj/home/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Example(models.Model):
+    name = models.CharField(max_length = 50, unique = False)
+    age = models.IntegerField()
+    number = models.IntegerField()
+
+    def __int__(self):
+        return self.number
+
+    class Meta:
+        verbose_name_plural = "Example"
+
+
+class Club(models.Model):
+    name = models.CharField(max_length = 50, unique = False)
+    country = models.CharField(max_length = 50)
+    trophies = models.IntegerField()
+    position = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Football Club"
+
+
+class Movie(models.Model):
+    movie = models.CharField(max_length = 50, unique = False)
+    director = models.CharField(max_length = 50, unique = False)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.movie
+
+    class Meta:
+        verbose_name_plural = "Film"
+
+
+class Game(models.Model):
+    game = models.CharField(max_length = 50, unique = False)
+    publisher = models.CharField(max_length = 50, unique = False)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.publisher
+
+    class Meta:
+        verbose_name_plural = "Game"
